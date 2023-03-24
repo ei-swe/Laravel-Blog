e4ebd3207da19f2f7d28c0a850d7521c10785498
eiswe <eiswecumwa@gmail.com>
2023-03-24 10:40:01 +0630

    laravel blog

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    //
+    public function index() 
+    {
+        $data = Article::latest()->paginate(5);
+
+        return view('articles.index', [
+            'articles' => $data
+        ]);
+    }
+
+    public function detail($id)
+    {
+        $data = Article::find($id);
+
+        return view('articles.detail', [
+            'article' => $data
+        ]);
+    }
+
+    public function add()
+    {
+        $data = [
+            ["id" => 1, "name" => "News"],
+            ["id" => 2, "name" => "Tech"],
+        ];
+
+        return view('articles.add', [
+            'categories' => $data
+        ]);
+    }
+
+    public function create ()
+    {
+        $article = new Article;
+        $article->title = request()->title;
+        $article->body = request()->body;
+        $article->category_id = request()->category_id;
+        $article->save();
+
+        return redirect('/articles');
+    }
+}
